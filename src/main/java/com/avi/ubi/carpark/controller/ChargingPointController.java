64003a49004f6a55f6d4badf82e6e38cc7d47885
Andrés Villarreal <andres@villarreal.co.cr>
2020-02-08 21:36:15 +0100

    CChanging CORS config

--- src/main/java/com/avi/ubi/carpark/controller/ChargingPointController.java
+++ src/main/java/com/avi/ubi/carpark/controller/ChargingPointController.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(("/chargingPoints"))
 
